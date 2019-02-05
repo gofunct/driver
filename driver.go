397a0487a4a78d4ddc363b7be222fd3850c80863
Coleman Word <coleman@Colemans-MacBook-Pro.local>
2019-02-05 12:09:06 -0700

    added methods

--- driver.go
+++ driver.go
@@ -2,23 +2,36 @@ package driver
 
 import (
 	"context"
+	"fmt"
 	"github.com/pkg/errors"
+	"os"
 )
 
+type InitFunc func()
+type HandlerFunc func() error
+
 type Driver interface {
-	Init() error
-	Runnable() bool
-	Run(ctx context.Context) error
+	Initializers() []InitFunc
+	Handlers() []HandlerFunc
+	Help() string
+	Debug()
+	Init(ctx context.Context, i ...InitFunc)
+	Runnable(ctx context.Context) bool
+	Run(ctx context.Context, h ...HandlerFunc) error
 }
 
 func Drive(ctx context.Context, d Driver) error {
-	if err := d.Init(); err != nil {
-		return err
+	switch os.Args[1] {
+	case "help", "--help", "-h", "h":
+		fmt.Println(d.Help())
+	case "debug", "--debug":
+		d.Debug()
 	}
-	if !d.Runnable() {
+	d.Init(ctx, d.Initializers()...)
+	if !d.Runnable(ctx) {
 		return errors.New("driver is currently not runnable")
 	}
-	if err := d.Run(ctx); err != nil {
+	if err := d.Run(ctx, d.Handlers()...); err != nil {
 		return errors.WithStack(err)
 	}
 	return nil
